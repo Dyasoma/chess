5dd607d682308351d6413a17032d7b66f9e51b31
Dyasoma <ddieasoma@gmail.com>
2024-08-26 17:08:33 -0600

    Added simple movement of pieces, including some instance methods for the board. Such as Moving a piece, Drawing a piece, Clearing a piece from the board e.t.c.

--- chess/board.py
+++ chess/board.py
@@ -1,6 +1,5 @@
 import pygame
 from .constants import BOARDPOSX, BOARDPOSY, SQUARESIZE
-from .square import Square
 from .piece import Piece
 
 
@@ -32,11 +31,11 @@ class Board:
         self.rect: pygame.Rect = self.__create_board_rect()
 
 
-    def __create_board_struct(self) -> list[list[Square]]:
+    def __create_board_struct(self) -> list[list]:
         """
         Creates the instance attribute "struct" for the board object of class Board.
         refers to the boards data structure, implemented as a list of lists
-        whose entries are objects of class Square. Mutates the list as the list is built.
+        Mutates the list as the list is built.
         board.struct[i][j] refers to the ith row, and the jth column of the board.
         Returns : a list of lists
         Side effect : creates instance attribute "struct" and mutates it
@@ -45,9 +44,7 @@ class Board:
         for row_index in range(self.square_count):
             row = []
             for col_index in range(self.square_count):
-                color = (0, 0, 0)
-                square = Square(SQUARESIZE, color, row_index, col_index)
-                row.append(square)  # creates a square and adds it to the struct
+                row.append(None)  # Each entry will be None. i.e. empty
             struct.append(row)
         return struct
 
@@ -57,23 +54,25 @@ class Board:
         creates the board surface, using the pygame Surface object class, and assigning the returned
         surface as an instance attribute of the board object.
         Creates a black surface.
-        Sets each squares color instance attribute.
         blits square from struct attribute onto surface attribute.
         Serves as the "image" of the board.
         Returns : pygame Surface object
         """
         # Surf is first the entire size of the board
         surf = pygame.Surface((self.width, self.height))
+
         # we go through the board and for each square, we "blit" onto the board the current square.
         for row_index in range(self.square_count):
             for col_index in range(self.square_count):
-                current_square: Square = self.struct[row_index][col_index]
                 # checks if square is even or odd, setting even to white and odd to black
                 if (row_index + col_index) % 2 == 0:
-                    current_square.update_color(self.color_dark)
+                    square = pygame.Surface((SQUARESIZE, SQUARESIZE))
+                    square.fill(self.color_dark)
+                    surf.blit(square, (col_index * SQUARESIZE, row_index * SQUARESIZE))
                 else:
-                    current_square.update_color(self.color_light)
-                surf.blit(current_square.surface, current_square.rel_pos)
+                    square = pygame.Surface((SQUARESIZE, SQUARESIZE))
+                    square.fill(self.color_light)
+                    surf.blit(square, (col_index * SQUARESIZE, row_index * SQUARESIZE))
         return surf
 
     def __create_board_rect(self) -> pygame.Rect:
@@ -85,7 +84,82 @@ class Board:
         """
         rec = self.surface.get_rect(topleft=(BOARDPOSX, BOARDPOSY))
         return rec
-    
+
+
+    def move_piece(self, piece : Piece, new_row : int, new_col : int):
+        """
+        move_piece(self, piece: Piece, new_row : int, new_col : int):
+        moves the piece on the boards data  structure.
+        updates the pieces row and column attributes. 
+        Then calls the draw piece instance method to draw the piece onto the board
+        returns : None
+        """
+        
+        # Update piece parameters
+        old_row = piece.row
+        old_col = piece.col
+        piece.row = new_row
+        piece.col = new_col
+        # Update board parameters
+        self.struct[old_row][old_col] = None
+        self.struct[new_row][new_col] = piece
+        
+        # draw piece onto new position 
+        self.draw_piece(piece, piece.row, piece.col)
+        # clear from previous position
+        self.clear_piece(piece, old_row, old_col)
+
+
+
+        # put new position
+        # update piece attributes
+
+        # draw the piece onto board
+
+    def grid_to_rel_pos(self, row : int, col : int) -> tuple:
+        """
+        grid_to_rel_pos(self, row : int, col : int) -> tuple:
+        converts the grid coordinates into a position relative to the board
+        returns : a tuple (x,y)
+        """
+        return (col * SQUARESIZE, row * SQUARESIZE)
+
+    def draw_piece(self, piece : Piece, row : int, col : int):
+        """
+        draw_piece(self, piece : Piece, row : int , col : int):
+        Draws the piece onto the board. 
+        returns : None
+        """
+        pos = self.grid_to_rel_pos(row, col)
+        self.surface.blit(piece.surface, pos)
+
+    def clear_piece(self, piece : Piece, row : int, col : int):
+        """
+        clear_piece(self, piece : Piece):
+        Clears the piece from the board. "Undrawing" it 
+        returns : None
+        """
+        pos = self.grid_to_rel_pos(row, col)
+        if (row + col) % 2 == 0:
+            self.color_square(self.color_dark, row, col)
+        else:
+            self.color_square(self.color_light, row, col)
+
+
+    def color_square(self, color : pygame.Color,  row : int, col : int):
+        """
+        color_square(self, color : pygame.Color,  row : int, col : int):
+
+        Colors the square based on the inputted color, given a row and column on the board.
+        creates a pygames surface, fills it with provided color and then blits it onto the board
+        surface. 
+        returns : None 
+        """
+        square = pygame.Surface((SQUARESIZE, SQUARESIZE))
+        square.fill(color)
+        self.surface.blit(square, (col * SQUARESIZE, row * SQUARESIZE))
+         
+
     def draw_board(self, window : pygame.Surface) -> None:
         """
         draw_board(self, window : pygame.Surface):
